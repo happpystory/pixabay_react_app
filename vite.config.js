20fb80aa9cc1b176b02cb071d0b7e5293c7175bc
Tisho <tishoyanchev@yahoo.com>
2024-09-15 17:56:42 +0200

    added homepage to package json

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import windicss from 'vite-plugin-windicss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), windicss()],
-  base: '/pixabay_react_app/',
+  base: '/pixabay_react_app/'
 })
