795a3d4140ff60b9fac4ec6e7da7cf9af4b63bb4
Tisho <tishoyanchev@yahoo.com>
2024-09-15 19:58:27 +0200

    removed base + new yml file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import windicss from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), windicss()],
-  base: '/pixabay_react_app/'
+  plugins: [react(), windicss()]
 })
