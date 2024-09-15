bb162d8b985b60b4c4134fe49551e0689c7bc9f9
Tisho <tishoyanchev@yahoo.com>
2024-09-15 23:00:24 +0200

    centralized the container + added spacing to cards

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,16 @@ function App() {
   }, [term])
 
   return (
-    <div className="container mx-auto">
+    <div className="flex flex-col items-center">
       <ImageSearch searchText={(text) => setTerm(text)} />
 
       {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
-
-     {isLoading 
-     ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> 
-     : <div className="grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> 
+      : <div className="flex flex-wrap px-10 pb-10 gap-10 w-11/12">
+          {images.map(image => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>}
     </div>
   );
 }
